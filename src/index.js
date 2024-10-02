56056ee7dbe8ffb421e7a88dde5a0ba458d554b8
Qudn1m <117811649+Qudn1m@users.noreply.github.com>
2024-10-02 16:17:55 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import HomePage from './App';
+import counterReducers from './store';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import ProductPage from './ProductPage';
+import AboutUs from './AboutUs';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HomePage />
+    <Provider store={counterReducers}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/app" element={<Home />} />
+          <Route path="/product-page" element={<ProductPage />} />
+          <Route path="/about-us" element={<AboutUs />} />
+        </Routes>
+      </Router>
+    </Provider>
   </React.StrictMode>
+  ,
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
